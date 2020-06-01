ac34686332be8d17aee5d04abec183c33a9c0864
Alejandro <diazale94@outlook.com>
2020-06-01 00:03:05 -0300

    primer commit

---
+++ src/components/Galeria.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="galeria">
+        <h1>{{ mensaje}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            mensaje: 'Este es un componente de galeria'
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+</style>
